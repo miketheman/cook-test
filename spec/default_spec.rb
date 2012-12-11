fcd03bdbb03df263210adae919b8a49c8bbdcd98
Mike Fiedler <miketheman@gmail.com>
2012-12-10 23:24:16 -0500

    Adding platform-specific chefspec/fauxhai tests

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,18 +1,55 @@
 require 'spec_helper'
 
 describe 'cook-test::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'cook-test::default' }
 
-  it 'should install the ntp package' do
-    chef_run.should install_package 'ntp'
+  context 'when using a debian-family distro' do
+    before do
+      Fauxhai.mock(platform: 'ubuntu', version: '12.04')
+    end
+
+    let (:chef_run) { ChefSpec::ChefRunner.new.converge 'cook-test::default' }
+
+    it 'installs the ntp package' do
+      chef_run.should install_package 'ntp'
+    end
+
+    it 'installs the ntpdate package' do
+      chef_run.should install_package 'ntpdate'
+    end
+
+    it 'enables and starts the ntp service' do
+      chef_run.should set_service_to_start_on_boot 'ntp'
+      chef_run.should start_service 'ntp'
+    end
+
+    it 'creates a configuration file' do
+      chef_run.should create_file '/etc/ntp.conf'
+    end
   end
 
-  it 'should enable the ntp service' do
-    chef_run.should set_service_to_start_on_boot 'ntp'
-  end
+  context 'when using a redhat-family distro above 6.x' do
+    before do
+      Fauxhai.mock(platform: 'centos', version: '6.3')
+    end
 
-  it 'should have a configuration file' do
-    chef_run.should create_file '/etc/ntp.conf'
+    let (:chef_run) { ChefSpec::ChefRunner.new.converge 'cook-test::default' }
+
+    it 'installs the ntp package' do
+      chef_run.should install_package 'ntp'
+    end
+
+    it 'installs the ntpdate package' do
+      chef_run.should install_package 'ntpdate'
+    end
+
+    it 'enables and starts the ntpd service' do
+      chef_run.should set_service_to_start_on_boot 'ntpd'
+      chef_run.should start_service 'ntpd'
+    end
+
+    it 'creates a configuration file' do
+      chef_run.should create_file '/etc/ntp.conf'
+    end
   end
 
 end
